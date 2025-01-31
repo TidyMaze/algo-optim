e35ede8d2ad5c16639f206f30f001125e2cca88e
yrolland <yann.rolland@iadvize.com>
2025-01-31 18:58:25 +0100

    wip

--- starter.py
+++ starter.py
@@ -124,7 +124,7 @@ def display_map(clients, tours, depth, score):
 
     # show each point on a scatter plot
     for client in clients:
-        ax.scatter(*client["position"], s=client["pizzas"]*10, alpha=0.5)
+        ax.scatter(*client["position"], s=(client["pizzas"]*3)**2, alpha=0.5)
 
     print(f"Tours count: {len(tours)}")
     print(f"Avg client per tour: {sum(len(t) for t in tours) / len(tours)}")
