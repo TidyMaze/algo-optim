841528ffdf2eee3f84be7a108186a474c86d415a
yrolland <yann.rolland@iadvize.com>
2025-02-03 22:23:04 +0100

    alex

--- starter.py
+++ starter.py
@@ -105,7 +105,7 @@ def optimize_2_opt(tour, clients):
                 new_dist = tour_distance(new_tour, clients)
                 old_dist = tour_distance(best_tour, clients)
                 if new_dist < old_dist:
-                    print(f"Tour distance improved (2 opt): {old_dist} -> {new_dist}")
+                    # print(f"Tour distance improved (2 opt): {old_dist} -> {new_dist}")
                     best_tour = new_tour
                     best_found = True
                     break
@@ -144,7 +144,7 @@ def optimize_3_opt(tour, clients):
                     new_dist = tour_distance(new_tour, clients)
                     old_dist = tour_distance(best_tour, clients)
                     if new_dist < old_dist:
-                        print(f"Tour distance improved (3 opt): {old_dist} -> {new_dist}")
+                        # print(f"Tour distance improved (3 opt): {old_dist} -> {new_dist}")
                         best_tour = new_tour
                         best_found = True
                         break
@@ -575,6 +575,107 @@ def solve_clarke_wright(clients):
 
     return res
 
+import random
+
+def next_delivery_candidates(
+        position: tuple[int, int],
+        clients: list[dict[str, any]],
+        available_space: int
+) -> list[dict[str, any]]:
+    clients = clients.copy()
+    # sort clients by distance
+    clients.sort(key=lambda client: manhattan_distance(position, client["position"]))
+    # filter such that we keep only if we have the capacity
+    clients = list(filter(lambda client: client["pizzas"] <= available_space, clients))
+    # first result
+    return clients[:1]
+
+def first_delivery_candidates(
+        position: tuple[int, int],
+        clients: list[dict[str, any]],
+        available_space: int
+) -> list[dict[str, any]]:
+    clients = clients.copy()
+    # sort clients by distance
+    clients.sort(key=lambda client: manhattan_distance(position, client["position"]), reverse=True)
+    # filter such that we keep only if we have the capacity
+    # clients = list(filter(lambda client: client["pizzas"] <= available_space, clients))
+    # first 10 results
+    return clients[:10]
+
+def pick_random_with_proba(clients, proba):
+    return random.choices(clients, proba)[0]
+
+def plan_tour(
+        clients: list[dict[str, any]]
+) -> list[int]:
+    available_space = 10
+    current_position = (0, 0)
+    current_tour = []
+    # clone array
+    clients = clients.copy()
+
+    while len(clients) > 0:
+        if available_space <= 0:
+            return current_tour
+
+        if len(current_tour) == 0:
+            candidates = first_delivery_candidates(current_position, clients, available_space)
+
+            # pick one random, with probability proportional to distance (closer is better)
+            neighbour = pick_random_with_proba(candidates, [1 / manhattan_distance(current_position, c["position"]) for c in candidates])
+        else:
+            candidates = next_delivery_candidates(current_position, clients, available_space)
+            if len(candidates) > 0:
+                neighbour = pick_random_with_proba(candidates, [1 / manhattan_distance(current_position, c["position"]) for c in candidates])
+            else:
+                neighbour = None
+        if not neighbour or manhattan_distance(current_position, neighbour["position"]) > manhattan_distance((0, 0), neighbour["position"]):
+            return current_tour
+        else:
+            current_position = neighbour["position"]
+            available_space -= neighbour["pizzas"]
+            clients.remove(neighbour)
+            current_tour.append(neighbour["id"])
+    return current_tour
+
+def plan_tours(clients: list[dict[str, any]]) -> str:
+    tours: list[list[int]] = []
+
+    while len(clients) > 0:
+        current_tour = plan_tour(clients)
+        tours.append(current_tour)
+        for client_id in current_tour:
+            clients = [client for client in clients if client["id"] != client_id]
+
+    tours_string = ""
+    for tour in tours:
+        tours_string += " ".join(map(str, tour)) + "\n"
+    return tours_string
+
+def evaluate_tour(clients):
+    tours_string = plan_tours(clients)
+    score, valid, message = get_score(tours_string)
+    return score, tours_string
+
+def solve_v5(clients: list[dict[str, any]]) -> str:
+    min_score = 999999
+    min_tour = ""
+
+    for i in range(10000):
+        # print(f"Generation {i}")
+
+        score, tours_string = evaluate_tour(clients)
+
+        if score < min_score:
+            min_score = score
+            min_tour = tours_string
+            print(f"New best score {min_score} at gen {i}")
+            score_validation = get_score(min_tour)
+            display_map(clients, [list(map(int, tour.split())) for tour in min_tour.strip().split("\n")], 0, min_score)
+
+
+    return [list(map(int, tour.split())) for tour in min_tour.strip().split("\n")]
 
 # Solution minimale : faire une tournée par c§lient
 def solve():
@@ -582,7 +683,7 @@ def solve():
     clients = load_clients("dataset.csv") # les clients sont sockés dans une liste de dict, avec pour clé "id", "position", "pizzas"
 
     # tours = solve_clarke_wright(clients)
-    tours = solve_beam_search(clients)
+    tours = solve_v5(clients)
 
     print(f"Adding tour")
 
