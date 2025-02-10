05fa04156507692522465f3dc928ca679c45b81a
yrolland <yann.rolland@iadvize.com>
2025-02-10 17:07:20 +0100

    wip

--- tsp-sa.py
+++ tsp-sa.py
@@ -128,7 +128,7 @@ def tsp_sa(clients):
 
     INITIAL_TEMP = 100
     temperature = INITIAL_TEMP
-    cooling_rate = 0.999999
+    cooling_rate = 0.9999999
 
     iteration = 0
     history = [(0, best_distance, temperature, 1)]
