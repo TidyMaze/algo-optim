26da1ea0f819dd8a28f4a21ab98d46d0745a07c9
yrolland <yann.rolland@iadvize.com>
2025-02-10 14:12:18 +0100

    wip

--- tsp-sa.py
+++ tsp-sa.py
@@ -129,9 +129,9 @@ def tsp_sa(clients):
 
     # simulated annealing
 
-    INITIAL_TEMP = 1
+    INITIAL_TEMP = 10
     temperature = INITIAL_TEMP
-    cooling_rate = 0.9999999
+    cooling_rate = 0.999999
 
     iteration = 0
     history = [(0, best_distance, temperature, 1)]
