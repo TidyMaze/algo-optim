464c203490cf5b9902978e17ad4d7dc8f92d8773
yrolland <yann.rolland@iadvize.com>
2025-02-10 10:54:08 +0100

    wip

--- tsp-sa.py
+++ tsp-sa.py
@@ -163,7 +163,7 @@ def tsp_sa(clients):
                 best_distance = new_cost
                 print(f"New best distance {best_distance} at temperature {temperature}")
                 history.append((iteration, best_distance, temperature, 1))
-                display_solution(clients, best_ever, history, probability_history)
+                # display_solution(clients, best_ever, history, probability_history)
                 # temperature = INITIAL_TEMP
         else:
             # if the new solution is worse, accept it with a probability
@@ -175,7 +175,7 @@ def tsp_sa(clients):
             probability_history.append((iteration, p, worse_selected))
             probability_history = probability_history[-10000:]
 
-        if iteration % 1000 == 0:
+        if iteration % 10000 == 0:
             display_solution(clients, best_ever, history, probability_history)
 
         temperature *= cooling_rate
